e1ad897f2ccb6fcb88fb30ff8ca6bc4b91de07d8
Jonathon Anderson <coding@jpanderson.dev>
2023-08-01 22:52:34 -0700

    0.4.1 asdfler

--- Formula/asdfler.rb
+++ Formula/asdfler.rb
@@ -1,8 +1,8 @@
 class Asdfler < Formula
   desc "Small Rust-based CLI for managing asdf plugins"
   homepage "https://github.com/tedious-tools/asdfler"
-  url "https://github.com/tedious-tools/asdfler/archive/refs/tags/0.4.0.tar.gz"
-  sha256 "81cb27c8ee53e9a9f9bee662899306f736df698e3f07b63aef2001bfb0a3616e"
+  url "https://github.com/tedious-tools/asdfler/archive/refs/tags/0.4.1.tar.gz"
+  sha256 "eadcc411a75b6b443187ac07913535eeac282c5bfee7774dbaf9a090ec057bb5"
   license "MIT"
 
   depends_on "rust" => :build
